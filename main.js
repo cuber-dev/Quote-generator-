dc4b325ce832ddd5b40e8393132c7150afeb13cf
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-17 22:35:02 +0530

    ggg

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const quoteText = document.querySelector('#quoteText');
+const author = document.querySelector('#author');
 const generateQuote = document.querySelector('#generateBtn');
 
 const url = 'https://api.quotable.io/random';
@@ -8,7 +9,9 @@ async function makeRequest(){
    try {
      let response = await fetch(url);
      let data = await response.json();
+     console.log(data);
      quoteText.innerText = data.content;
+     author.innerText = '-' + data.author;
    } catch (e) {
      quoteText.innerText = "An error occurred while generating the quote. Please try again later.";
    }
