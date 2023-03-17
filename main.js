039135e31a2bb1228288b8bfca9468b0e82f833c
adepunandhu9@gmail.com <adepunandhu9@gmail.com>
2023-03-18 01:41:08 +0530

    hchhcc

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
+const quoteBox = document.querySelector('#quoteBox');
 const quoteText = document.querySelector('#quoteText');
 const author = document.querySelector('#author');
-const generateQuote = document.querySelector('#generateBtn');
+const generateQuoteBtn = document.querySelector('#generateBtn');
 
 const url = 'https://api.quotable.io/random';
-generateQuote.addEventListener('click', makeRequest);
-
 async function makeRequest(){
    try {
      let response = await fetch(url);
@@ -16,6 +15,35 @@ async function makeRequest(){
    }
 }
 
+generateQuoteBtn.addEventListener('click', makeRequest);
+
+quoteBox.addEventListener('click',(e) => {
+  
+  // copy
+  const text = document.createElement('textarea');
+  text.value = quoteBox.innerText;
+  document.body.append(text);
+  text.select();
+  document.execCommand('copy');
+  document.body.removeChild(text);
+  
+  
+  // Create span 
+  const span = document.createElement('span');
+  span.innerText = "Copied to Clipboard!";
+  span.classList.add('copy');
+  
+  // Append 
+  document.body.append(span);
+  
+  // Remove
+  setTimeout(() => {
+    document.body.removeChild(span);
+  },3500);
+  
+});
+
+// Onload
 window.addEventListener('load',() => {
   makeRequest();
 });
